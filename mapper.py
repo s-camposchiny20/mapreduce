6f307f06d7dd426be57fc9f6101e1db182df66e4
s-camposchiny20 <s_camposchiny20@stud.hwr-berlin.de>
2021-05-26 20:57:41 +0200

    Ex. 3: Raise Exception

--- mapper.py
+++ mapper.py
@@ -34,11 +34,14 @@ for line in sys.stdin:
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
 
+    if len(data) != 6:
+        raise ValueError("There are not six elements in the tuple.")
+
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
 
     # Write the key-value combination to standard output (stdout)
-    # Key is the payment, value is the sales     
+    # Key is the payment, value is the sales
     # With a tab (\t) between key and value
     # New line \n means new record
     sys.stdout.write("{0}\t{1}\n".format(category, sales))
